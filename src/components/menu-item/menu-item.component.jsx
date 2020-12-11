4f6c2056f096d96d5cdb91fb233689bbbb2395c7
Fede del Rincon <delrincon@gmail.com>
2020-12-11 10:43:48 -0300

    069C-  broken homepage to smaller + finish styling

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,12 +4,15 @@ import './menu-item.styles.scss';
 
 //podria usar (props), pero uso ({title}) porque lo destructuro
 const MenuItem = ({ title, imageUrl, size }) => (  //si tiene el size lo usa, sino no pasa nada
-    <div style={{
-        backgroundImage: `url(${imageUrl})`
-    }}
-     className={`${size} menu-item`}>
+    <div className={`${size} menu-item`}>
+        <div 
+            className='background-image' 
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
         <div className='content'>
-            <h1 className='title'>{title}</h1>
+            <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
