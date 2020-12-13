d10b53e6963e873ef928e315674709089988debe
Fede del Rincon <delrincon@gmail.com>
2020-12-13 13:20:45 -0300

    081B- adding footer component

---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './footer.styles.scss';
+
+const Footer = () => (
+    <div className='footer'>Copyright&copy; FDR 2020. All rigths reserved. </div>
+);
+
+export default Footer;
