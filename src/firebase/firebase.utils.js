128a3911e672dcb79d9cdb28cc1e779aa5ad1f62
Fede del Rincon <delrincon@gmail.com>
2020-12-15 14:30:54 -0300

    096a- google sign in authentication 3 NO CAMBIA SIGN OUT

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,12 +13,9 @@ const config = {
 };
 
 firebase.initializeApp(config);
-
 export const auth = firebase.auth();
-export const firestore = firebase.firestore();
-
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account'});
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
-
-export default firebase;
+export const firestone = firebase.firestore();
+ 
+const provider= new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt: 'select_account'});
+export const signInWithGoogle =()=> auth.signInWithPopup(provider);
