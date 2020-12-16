4cb0a5b95f6adef8f21c09d09580d8249307fb15
Fede del Rincon <delrincon@gmail.com>
2020-12-16 14:53:39 -0300

    101a- storing User Dara in The App

--- src/App.js
+++ src/App.js
@@ -22,8 +22,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user); //ahora no va a funcionar lo de login/logout xq saque lo de set.state
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });
+      } else {
+        this.setState({currentUser: userAuth});
+      }
     });
   }
 
