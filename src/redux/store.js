16a7413d29c41744c5a058438fe7d2e52e4c4ebb
“fededelrincon” <delrincon@gmail.com>
2020-12-27 22:29:24 -0300

    156c- take out persistor + show pay boton only if it log

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import { persistStore } from 'redux-persist';
+//import { persistStore } from 'redux-persist'; //si quiera mantener lo que compre al hacer
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
@@ -8,6 +8,6 @@ const middlewares = [logger];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export const persistor = persistStore(store);
+//export const persistor = persistStore(store);
 
 //export default { store, persistor };
