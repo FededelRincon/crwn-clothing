ee2db6536ea6984dc2670f6e6122435b457397d0
“fededelrincon” <delrincon@gmail.com>
2020-12-28 19:56:49 -0300

    161d- vuelvo a usar mi store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,16 @@
-import { applyMiddleware, compose, createStore } from "redux";
-
+import { createStore, applyMiddleware } from 'redux';
 //import { persistStore } from 'redux-persist'; //si quiera mantener lo que compre al hacer
-import logger from "redux-logger";
-import rootReducer from "./root-reducer";
+import logger from 'redux-logger';
+
+import rootReducer from './root-reducer';
 
 const middlewares = [];
 
-if (process.env.NODE_ENV === "development") {
-  // para que deploy no tire las cosas por consola
+if (process.env.NODE_ENV === 'development') { // para que deploy no tire las cosas por consola
   middlewares.push(logger);
 }
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-export const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(...middlewares))
-);
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 //export const persistor = persistStore(store);
 
