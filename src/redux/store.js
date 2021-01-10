aefd5b66d93a5e60f3326fe01dbde5f69b289c5a
“fededelrincon” <delrincon@gmail.com>
2021-01-10 10:46:27 -0300

    update store to use Redux_Devtools_Extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,22 @@
-import { createStore, applyMiddleware } from 'redux';
-//import { persistStore } from 'redux-persist'; //si quiera mantener lo que compre al hacer
-import logger from 'redux-logger';
+import { applyMiddleware, compose, createStore } from "redux";
 
-import rootReducer from './root-reducer';
+//import { persistStore } from 'redux-persist'; //si quiera mantener lo que compre al hacer
+import logger from "redux-logger";
+import rootReducer from "./root-reducer";
 
 const middlewares = [];
 
-if (process.env.NODE_ENV === 'development') { // para que deploy no tire las cosas por consola
+if (process.env.NODE_ENV === "development") {
+  // para que deploy no tire las cosas por consola
   middlewares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middlewares))
+);
 
 //export const persistor = persistStore(store);
 
